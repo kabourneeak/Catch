f3299bbac6b14aa90d5db2fa6f459c1bc437ad5c
Gregory Bint <kabourneeak@gmail.com>
2017-07-19 20:48:33 -0400

    Added simple min heap

---
+++ CatchLibrary/Heap/MinHeap.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CatchLibrary.Heap
+{
+    public class MinHeap<TP, TV> where TP : IComparable
+    {
+        private const int DefaultHeapSize = 16;
+
+        private TP[] _heapPriorities;
+        private TV[] _heapValues;
+        private int _count;
+
+        public MinHeap()
+        {
+            _heapPriorities = new TP[DefaultHeapSize];
+            _heapValues = new TV[DefaultHeapSize];
+            _count = 0;
+        }
+
+        public void Add(TP priority, TV value)
+        {
+            if (_count == _heapPriorities.Length)
+                Double();
+
+            // add new value to end heap
+            var cur = _count;
+            _heapPriorities[cur] = priority;
+            _heapValues[cur] = value;
+
+            // fixup heap property
+            while (cur > 0)
+            {
+                var parent = ParentIndex(cur);
+
+                // if current node is smaller than parent, then move current node closer to root
+                if (_heapPriorities[cur].CompareTo(_heapPriorities[parent]) < 0)
+                {
+                    Swap(cur, parent);
+                    cur = parent;
+                }
+                else
+                {
+                    break;
+                }
+            }
+
+            // indicate updated size of heap
+            _count += 1;
+        }
+
+        private int ParentIndex(int childIndex) => (childIndex + 1) / 2 - 1;
+
+        private int LeftChildIndex(int parentIndex) => (parentIndex + 1) * 2 - 1;
+
+        private int RightChildIndex(int parentIndex) => (parentIndex + 1) * 2;
+
+        private void Swap(int cur, int parent)
+        {
+            var tp = _heapPriorities[cur];
+            _heapPriorities[cur] = _heapPriorities[parent];
+            _heapPriorities[parent] = tp;
+
+            var tv = _heapValues[cur];
+            _heapValues[cur] = _heapValues[parent];
+            _heapValues[parent] = tv;
+        }
+
+        public int Count() => _count;
+
+        public bool IsEmpty() => _count == 0;
+
+        public TV Extract(out TP priority)
+        {
+            if (_count == 0)
+                throw new IndexOutOfRangeException();
+
+            // extract top of heap, and replace with last element
+            var val = _heapValues[0];
+            priority = _heapPriorities[0];
+            var cur = _count--;  // post-decrement
+            Swap(0, cur);
+
+            // fixup heap property
+            while (true)
+            {
+                var left = LeftChildIndex(cur);
+                var right = RightChildIndex(cur);
+                var smallest = cur;
+
+                if (left < _count && _heapPriorities[left].CompareTo(_heapPriorities[smallest]) < 0)
+                    smallest = left;
+                if (right < _count && _heapPriorities[right].CompareTo(_heapPriorities[smallest]) < 0)
+                    smallest = right;
+
+                if (smallest != cur)
+                    Swap(cur, smallest);
+                else
+                    break;
+            }
+            
+            return val;
+        }
+
+        public TV Peek()
+        {
+            if (_count == 0)
+                throw new IndexOutOfRangeException();
+
+            return _heapValues[0];
+        }
+
+        public TP PeekPriority()
+        {
+            if (_count == 0)
+                throw new IndexOutOfRangeException();
+
+            return _heapPriorities[0];
+        }
+
+        private void Double()
+        {
+            var newHeapPriorities = new TP[_heapPriorities.Length * 2];
+            var newHeapValues = new TV[_heapValues.Length * 2];
+
+            _heapPriorities.CopyTo(newHeapPriorities, 0);
+            _heapValues.CopyTo(newHeapValues, 0);
+
+            _heapPriorities = newHeapPriorities;
+            _heapValues = newHeapValues;
+        }
+    }
+}
---
+++ CatchTests/MinHeapTests.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CatchLibrary.Heap;
+using Microsoft.VisualStudio.TestPlatform.UnitTestFramework;
+
+namespace CatchTests
+{
+    [TestClass]
+    public class MinHeapTests
+    {
+        private MinHeap<float, object> CreateTestSubject()
+        {
+            return new MinHeap<float, object>();
+        }
+            
+        [TestMethod]
+        public void ConstructorTest()
+        {
+            var subject = CreateTestSubject();
+        }
+
+        [TestMethod]        
+        public void EmptyHeapCannotPeek()
+        {
+            var subject = CreateTestSubject();
+            Assert.ThrowsException<IndexOutOfRangeException>(() => subject.Peek());
+        }
+
+        [TestMethod]
+        public void EmptyHeapCannotExtract()
+        {
+            var subject = CreateTestSubject();
+            Assert.ThrowsException<IndexOutOfRangeException>(() => subject.Extract(out _));
+        }
+
+        [TestMethod]
+        public void EmptyHeapReportsEmpty()
+        {
+            var subject = CreateTestSubject();
+
+            Assert.IsTrue(subject.IsEmpty());
+            Assert.AreEqual(0, subject.Count());
+        }
+
+        [TestMethod]
+        public void InsertInOrder()
+        {
+            var subject = CreateTestSubject();
+
+            var firstObject = new object();
+            subject.Add(1.0f, firstObject);
+            Assert.IsTrue(object.ReferenceEquals(firstObject, subject.Peek()));
+
+            for(int i = 2; i < 100; ++i)
+            {
+                subject.Add(i, new object());
+
+                // first object stays at the front
+                Assert.IsTrue(object.ReferenceEquals(firstObject, subject.Peek()));
+            }
+        }
+
+        [TestMethod]
+        public void InsertReverseOrder()
+        {
+            var subject = CreateTestSubject();
+
+            for (int i = 100; i > 0; --i)
+            {
+                var obj = new object();
+                subject.Add(i, obj);
+
+                // most recently added object goes to the front
+                Assert.IsTrue(object.ReferenceEquals(obj, subject.Peek()));
+            }
+        }
+    }
+}
