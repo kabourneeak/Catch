686f99eae906d631f1559411e16c13447794a776
Gregory Bint <kabourneeak@gmail.com>
2017-07-24 20:58:40 -0400

    PathMobBehaviour now unregisters itself at end of path

--- Catch/Mobs/PathMobBehaviour.cs
+++ Catch/Mobs/PathMobBehaviour.cs
@@ -12,23 +12,23 @@ namespace Catch.Mobs
             Advancing, EndOfPath, Removed
         }
 
-        private readonly AgentBase _mob;
+        private readonly AgentBase _agent;
         private readonly MapPath _mapPath;
         private PathMobBehaviourStates _state;
         private int _pathIndex;
         private float _velocity;
 
-        public PathMobBehaviour(AgentBase mob, MapPath mapPath, float velocity)
+        public PathMobBehaviour(AgentBase agent, MapPath mapPath, float velocity)
         {
-            _mob = mob;
+            _agent = agent;
             _mapPath = mapPath;
             _velocity = velocity;
             _state = PathMobBehaviourStates.Advancing;
 
             _pathIndex = 0;
-            _mob.Tile = _mapPath[_pathIndex];
-            _mob.TileProgress = 0.5f; // start in the center of our source tile
-            _mob.Tile.AddMob(_mob);
+            _agent.Tile = _mapPath[_pathIndex];
+            _agent.TileProgress = 0.5f; // start in the center of our source tile
+            _agent.Tile.AddMob(_agent);
         }
 
         public float Update(IUpdateEventArgs e)
@@ -50,23 +50,23 @@ namespace Catch.Mobs
         private float UpdateAdvancing(IUpdateEventArgs e)
         {
             // advance through tile
-            _mob.TileProgress += _velocity * e.Ticks;
+            _agent.TileProgress += _velocity * e.Ticks;
 
             // advance to next tile, if necessary
-            while (_mob.TileProgress > 1 && _pathIndex < (_mapPath.Count - 1))
+            while (_agent.TileProgress > 1 && _pathIndex < (_mapPath.Count - 1))
             {
                 // leave old tile
-                _mob.Tile.RemoveMob(_mob);
+                _agent.Tile.RemoveMob(_agent);
 
                 // move to next tile
                 _pathIndex += 1;
-                _mob.TileProgress -= 1.0f;
-                _mob.Tile = _mapPath[_pathIndex];
-                _mob.Tile.AddMob(_mob);
+                _agent.TileProgress -= 1.0f;
+                _agent.Tile = _mapPath[_pathIndex];
+                _agent.Tile.AddMob(_agent);
             }
 
             // are we done?
-            if (_pathIndex == (_mapPath.Count - 1) && _mob.TileProgress >= 0.5f)
+            if (_pathIndex == (_mapPath.Count - 1) && _agent.TileProgress >= 0.5f)
                 _state = PathMobBehaviourStates.EndOfPath;
 
             // calculate Position
@@ -77,7 +77,7 @@ namespace Catch.Mobs
 
         private float UpdateEndOfPath(IUpdateEventArgs e)
         {
-            OnRemove();
+            e.Manager.Remove(this._agent);
 
             return 0.0f;
         }
@@ -87,24 +87,24 @@ namespace Catch.Mobs
             Vector2 prev;
             Vector2 next;
 
-            if (_mob.TileProgress < 0.5)
+            if (_agent.TileProgress < 0.5)
             {
-                next = _mob.Tile.Position;
+                next = _agent.Tile.Position;
                 prev = (_pathIndex > 0) ? _mapPath[_pathIndex - 1].Position : next;
-                _mob.Position = Vector2.Lerp(prev, next, 0.5f + _mob.TileProgress);
+                _agent.Position = Vector2.Lerp(prev, next, 0.5f + _agent.TileProgress);
             }
             else
             {
-                prev = _mob.Tile.Position;
+                prev = _agent.Tile.Position;
                 next = (_pathIndex < _mapPath.Count - 1) ? _mapPath[_pathIndex + 1].Position : prev;
-                _mob.Position = Vector2.Lerp(prev, next, _mob.TileProgress - 0.5f);
+                _agent.Position = Vector2.Lerp(prev, next, _agent.TileProgress - 0.5f);
             }
         }
 
         public void OnRemove()
         {
             _state = PathMobBehaviourStates.Removed;
-            _mob.IsActive = false;
+            _agent.IsActive = false;
         }
 
         public void OnHit(AttackModel incomingAttack)
